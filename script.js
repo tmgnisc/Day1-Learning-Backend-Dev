0d317a78f9f87c5aaddbb5cf57f67e60bc7b484a
Nischal <tamangnischal2018@gmail.com>
2024-07-29 19:01:31 +0545

    learning forEach

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ var arr = [1,2,3,4]
 arr.forEach(function(val){
     console.log(val + "hello")
 })
+
+let sum = 0;
+const numbers= [1,2,3,4,5]
+numbers.forEach(vunte)
+function vunte(item){
+    sum = item +1;
+    console.log(sum)
+}
