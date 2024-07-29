d82c552b6103b25801bf8d136bd591958976fee3
Nischal <tamangnischal2018@gmail.com>
2024-07-29 18:56:45 +0545

    forEach

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var arr = [1,2,3,4]
 //foreach map filter find indexOf
-arr.forEach(vunte)
-function vunte(arr, item, index){
-    arr[index] = item * 10;
-}
-console.log(arr)
+arr.forEach(function(val){
+    console.log(val + "hello")
+})
